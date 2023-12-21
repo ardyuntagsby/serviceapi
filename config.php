b18794b8ba86bc68c36c23d0171ee830b47f9ff1
ardyuntagsby <99413353+ardyuntagsby@users.noreply.github.com>
2023-12-21 13:09:35 +0700

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host = 'localhost';
-$username = 'root';
-$password = '';
-$database = 'service';
-
-$con = mysqli_connect($host, $username, $password, $database) or die ('unable to connect');
-?>
